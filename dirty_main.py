4b082f2afc386fa53a1fc77a56ff9f2d84435b01
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-05-20 16:38:09 +0300

    Using dirty main

--- dirty_main.py
+++ dirty_main.py
@@ -1,10 +1,11 @@
 import datetime
 import sys
-
 sys.path.insert(1, 'application')
 sys.path.insert(2, 'application/db')
-from salary import calculate_salary
-from people import get_employees, get_phone_by_name, number_1, time_zone, valid
+from salary import *
+from people import *
+from datetime import *
+
 
 
 date = datetime.datetime.today()

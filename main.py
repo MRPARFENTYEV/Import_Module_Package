761c56d9d73c340e5fc3c94f520ad1a2bdbdf25e
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-05-22 15:48:17 +0300

    Программа должна работать без этого кода.

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import datetime
-import sys
+from application import salary
+from application.db import people
 
-sys.path.insert(1, 'application')
-sys.path.insert(2, 'application/db')
-from salary import calculate_salary
-from people import get_employees, get_phone_by_name, number_1, time_zone, valid
 
 
 date = datetime.datetime.today()
 if __name__ == '__main__':
-    print(calculate_salary())
-    print(''.join(get_employees('Product manager')))
-    print(get_phone_by_name(get_employees('Product manager')))
-    print(number_1)
-    print(''.join(time_zone))
-    print(f'Телефон активен: {valid}')
+    print(salary.calculate_salary())
+    print(''.join(people.get_employees('Product manager')))
+    print(people.get_phone_by_name(people.get_employees('Product manager')))
+    print(people.number_1)
+    print(''.join(people.time_zone))
+    print(f'Телефон активен: {people.valid}')
     print(date)
